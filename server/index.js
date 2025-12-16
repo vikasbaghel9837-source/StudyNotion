28bd631de0d901f526b94396eade049b2c3304a0
Vikas <vikasbaghel9837@gmail.com>
2025-12-16 13:42:24 +0530

    Changes in frontend url

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
 	cors({
 		origin:[
 			"http://localhost:3000",
-			"https://study-notion-neon-ten.vercel.app"
+			"https://study-notion-phi-kohl.vercel.app"
 		], 
 		credentials:true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
